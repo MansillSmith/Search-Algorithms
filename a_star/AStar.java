17384dbf312657bed3cd27016fc2275fe4cc2c93
MansillSmith <mansill.smith@xtra.co.nz>
2020-05-21 20:16:00 +1200

    AStar: general stuff happening

--- a_star/AStar.java
+++ a_star/AStar.java
@@ -7,24 +7,58 @@ ID: 1350168
 
 Solve a given text file map using the A* method
 */
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class AStar{
     static int numInputs = 1;
 
+    //A queue which orders its contents based on a value
+    private class PriorityQueue{
+        private ArrayList<Path> queue;
+
+        public PriorityQueue(){
+            queue = new ArrayList<Path>();
+        }
+
+        //Adds a new path to the queue
+        public void Add(Path p){
+            double value = p.GetTotalCost();
+            for (int i = 0; i < queue.size(); i++){
+                //If the added path should go here
+                if (value < queue.get(i).GetTotalCost()){
+                    queue.add(i,p);
+                    return;
+                }
+            }
+            //The cost of this path was greater than all other paths currently in the queue
+            queue.add(p);
+        }
+
+        //Removes the first path from the queue
+        public Path Remove(){
+            return queue.remove(0);
+        }
+
+        public ArrayList<Path> GetListOfPaths(){
+            return queue;
+        }
+    }
+
     //Records a state
     private class State{
+        private String character;
         private int x;
         private int y;
-        private double distFromGoal;
+        public double distFromGoal;
 
         //Constructs a State
-        public State(int x, int y, int distFromGoal){
+        public State(int x, int y, String character){
             this.x = x;
             this.y = y;
-            this.distFromGoal = distFromGoal;
+            this.distFromGoal = 0;
+            this.character = character;
         }
 
         public int GetX(){
@@ -35,24 +69,139 @@ public class AStar{
             return y;
         }
 
-        public double GetDistFromGoal(){
-            return distFromGoal;
+        public String GetCharacter(){
+            return character;
+        }
+
+        public String SetCharacter(String character){
+            return this.character = character;
+        }
+
+        //Expands the state into the frontier
+        public void Expand(PriorityQueue frontier, Path thisPath, ArrayList<ArrayList<State>> map){
+            //Generate the new paths
+            ArrayList<Path> newPaths = new ArrayList<Path>();            
+
+            //
+            //Check if the values are too large or too small
+            //
+
+            newPaths.add(new Path(thisPath, map.get(this.GetY()).get(this.GetX() -1)));
+            newPaths.add(new Path(thisPath, map.get(this.GetY()).get(this.GetX() + 1)));
+            newPaths.add(new Path(thisPath, map.get(this.GetY() -1).get(this.GetX())));
+            newPaths.add(new Path(thisPath, map.get(this.GetY() +1).get(this.GetX())));
+
+            ArrayList<Integer> pathsToRemove = new ArrayList<Integer>();
+            //Check if the paths are valid
+            for(int i = 0; i < newPaths.size(); i++){
+                //remove the invalid paths
+                if(!newPaths.get(i).IsValid(frontier)){
+                    pathsToRemove.add(i);
+                }
+            }
+
+            //Removes the invalid paths
+            for(int i = 0; i < pathsToRemove.size(); i++){
+                newPaths.remove(pathsToRemove.get(i));
+            }
+
+            //Add the new paths to the frontier
+            for(int i = 0; i < newPaths.size(); i++){
+                frontier.Add(newPaths.get(i));
+            }              
+        }
+
+        //Checks if the State is a valid move
+        public Boolean IsValidMove(){
+            return (GetCharacter().equals(" ") || GetCharacter().equals("S") || GetCharacter().equals("G"));
+        }
+
+        @Override
+        public String toString(){
+            return GetX() + ", " + GetY() + "," + GetCharacter();
         }
     }
 
     //Records a path
     private class Path{
-        private LinkedList<State> listOfStates;
+        private ArrayList<State> listOfStates;
 
         //Constructs a path
         public Path(State s){
-            listOfStates = new LinkedList<State>();
+            listOfStates = new ArrayList<State>();
             listOfStates.add(s);
+        }
+
+        //Create a new path from another path
+        public Path(Path p, State s){
+            listOfStates = p.GetListOfStates();
+            this.AddState(s);
+        }
+
+        public ArrayList<State> GetListOfStates(){
+            return listOfStates;
         }
 
         //Adds another state to a path
         public void AddState(State s){
             listOfStates.add(s);
+        }
+
+        //Calculates the total cost of the Path
+        public double GetTotalCost(){
+            return listOfStates.size() + GetLastState().distFromGoal;
+        }
+
+        //Calculates if the path contains the goal
+        public Boolean ContainsGoal(){
+            return GetLastState().GetCharacter().equals("G");
+        }
+
+        //Expands the path
+        public void Expand(PriorityQueue frontier, ArrayList<ArrayList<State>> map){
+            GetLastState().Expand(frontier, this, map);
+        }
+
+        //Calculates if the path is valid
+        public Boolean IsValid(PriorityQueue frontier){
+            //If the final state of the path is a valid move
+            //And if the path does not contain a loop
+            //And the path is unique
+            return (GetLastState().IsValidMove() && !DoesThePathLoop() && UniquePath(frontier));
+        }
+
+        private State GetLastState(){
+            return listOfStates.get(listOfStates.size() -1);
+        }
+
+        //Checks if the path loops on itself
+        private Boolean DoesThePathLoop(){
+            State s = listOfStates.get(listOfStates.size() -1);
+            for(int i = 0; i < listOfStates.size() - 1; i++){
+                if(s.equals(listOfStates.get(i))){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //Checks that no paths overlap
+        private Boolean UniquePath(PriorityQueue frontier){
+            ArrayList<Path> listOfPaths = frontier.GetListOfPaths();
+            for(int i = 0; i < listOfPaths.size(); i++){
+                //If the last state of this path is contained in any other path
+                if(listOfPaths.get(i).listOfStates.contains(this.GetLastState())){
+                    //Remove the path with the highest total cost, return if this had the lowest
+                    if(this.GetTotalCost() < listOfPaths.get(i).GetTotalCost()){
+                        listOfPaths.remove(i);
+                        return true;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+            return true;
         }
     }
 
@@ -62,16 +211,40 @@ public class AStar{
             System.err.println("Please input the txt map file");
         }
         else{
-            LinkedList<String> masterList = new LinkedList<String>();
+            ArrayList<ArrayList<State>> map = new ArrayList<ArrayList<State>>();
             String mapFile = args[0];
+
+            //Finds the start and goal state
+            State goalState = null;
+            State startState = null;
+
+            AStar astar = new AStar();
 
             try{
                 //Opens the file
                 BufferedReader reader = new BufferedReader(new FileReader(mapFile));
+                int numlines = 0;
+
                 String line = reader.readLine();
                 while(line != null){
-                    masterList.add(line);
+                    ArrayList<State> temp = new ArrayList<State>();
+                    for(int i = 0; i < line.length(); i++){
+                        String c = Character.toString(line.charAt(i));
+
+                        //Sets a special reference to the start and end states
+                        State s = astar.new State(i, numlines, c);
+                        if(c.equals("G")){
+                            goalState = s;
+                        }
+                        else if(c.equals("S")){
+                            startState = s;
+                        }
+                        temp.add(s);
+
+                    }
+                    map.add(temp);
                     line = reader.readLine();
+                    numlines ++;
                 }
                 reader.close();
             }
@@ -79,63 +252,94 @@ public class AStar{
                 System.err.println("Error: " + e);
             }
 
-            //PrintMap2(masterList);
-
-            AStar astar = new AStar();
-            //Initialises the map list
-            final State[][] map = astar.new State[masterList.size()][masterList.get(0).length()];
-
-            //Finds the start and goal state
-            State goalState;
-            State startState;
-            for(int i=0; i < masterList.size(); i++){
-                String s = masterList.get(i);
-                for(int j=0; j < s.length(); j++){
-                    if (s.charAt(j).equals('G')){
-                        goalState = astar.new State(j,i,0);
-                    }
-                    else if(s.charAt(j).equals('S')){
-                        startState = astar.new State(j,i,0);
-                    }
+            //Set all the heuristic value for the states
+            for (int y = 0; y < map.size(); y++){
+                ArrayList<State> listOfStates = map.get(y);
+                for(int x = 0; x < listOfStates.size(); x++){
+                    State thisState = listOfStates.get(x);
+                    thisState.distFromGoal = CalculateDistance(thisState.GetX(), thisState.GetY(), goalState.GetX(), goalState.GetY());
                 }
             }
 
-            //Will set the start state distance from the goal correctly
-            //As the start state may have been found before the goal state
-            if(startState.distFromGoal == 0){
-                int thisX = startState.GetX;
-                int thisY = startState.GetY;
+            //Stores the frontier of the search algorithm
+            PriorityQueue frontier = astar.new PriorityQueue();
+            //Initialise the frontier
+            frontier.Add(astar.new Path(startState));
 
-                startState = astar.new State(thisX, thisY, CalculateDistance(thisX, thisY, goalState.GetX, goalState.GetY));
-            }
+            Path correctPath = null;
+            //Loop until the answer has been found
+            while (correctPath == null){
+                //Get a path from the frontier
+                Path p = frontier.Remove();
 
-            //Writes the linkedlist into a 2d array
-            for(int i=0; i < masterList.size(); i++){
-                String s = masterList.get(i);
-                for(int j=0; j < s.length(); j++){
-                    map[i][j] = astar.new State(j,i, CalculateDistance(j,i,goalState.GetX, goalState.GetY));
+                //Check that this path doesn't contain the goal
+                if(p.ContainsGoal()){
+                    correctPath = p;
+                }
+                else{
+                    p.Expand(frontier, map);
+                    PrintFrontier(frontier);
+                    break;
                 }
             }
+
+            if(correctPath != null){
+                ArrayList<State> listOfCorrectStates = correctPath.listOfStates;
+                for(int i = 0; i< listOfCorrectStates.size(); i++){
+                    if(!listOfCorrectStates.get(i).GetCharacter().equals("G") && !listOfCorrectStates.get(i).GetCharacter().equals("S")){
+                        listOfCorrectStates.get(i).SetCharacter("·");
+                    }
+                }
+            }
+            else{
+                System.err.println("A Path was not found");
+            }
+            
+
+
             PrintMap(map);
+            //PrintHeuristic(map);
         }
     }
 
-    public static void PrintMap2(LinkedList<String> map){
-        for(int i = 0; i < map.size(); i++){
-            System.out.println(map.get(i));
+    private static void PrintFrontier(PriorityQueue frontier){
+        ArrayList<Path> listOfPaths = frontier.GetListOfPaths();
+        for(int i = 0; i <listOfPaths.size(); i++){
+            ArrayList<State> listOfStates = listOfPaths.get(i).listOfStates;
+            for(int j = 0; j < listOfStates.size(); j++){
+                System.out.print(listOfStates.get(j).toString() + " | ");
+            }
+            System.out.println();
         }
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 
-    public static void PrintMap(String[][] map){
-        for(int i=0; i< map.length; i++){
-            for(int j=0; j< map[0].length; j++){
-                System.out.print(map[i][j]);
+    public static void PrintMap(ArrayList<ArrayList<State>> list){
+        for (int y = 0; y < list.size(); y++){
+            ArrayList<State> listOfStates = list.get(y);
+            for(int x = 0; x < listOfStates.size(); x++){
+                State thisState = listOfStates.get(x);
+                System.out.print(thisState.GetCharacter());
             }
             System.out.println();
         }
     }
 
-    private static void CalculateDistance(int x1, int y1, int x2, int y2){
-        Math.sqrt((x1-x2) * (x1-x2) + (y1-y2) * (y1-y2));
+    public static void PrintHeuristic(ArrayList<ArrayList<State>> list){
+        for (int y = 0; y < list.size(); y++){
+            ArrayList<State> listOfStates = list.get(y);
+            for(int x = 0; x < listOfStates.size(); x++){
+                State thisState = listOfStates.get(x);
+                System.out.print(Math.round(thisState.distFromGoal) + ",");
+            }
+            System.out.println();
+        }
+    }
+
+    //Calculates the distance from a given state to the goal state
+    private static double CalculateDistance(int x1, int y1, int x2, int y2){
+        return Math.sqrt((x1-x2) * (x1-x2) + (y1-y2) * (y1-y2));
     }
 }
